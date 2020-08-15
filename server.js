3fac0a8a51107ad6cc4789cbe6793b2bc253e528
Hevnie <fy.morishita@gmail.com>
2020-08-14 21:15:45 -0300

    changed to use riot api

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const opgg = require('opgg-scrape');
+const request = require('request');
+const https = require('https');
 const ejs = require('ejs');
+const config = require('config');
+require('dotenv').config();
+const axios = require('axios');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -9,32 +14,61 @@ app.set('view engine', 'ejs');
 app.use(express.json({ extended: false }));
 app.use(express.static('public'));
 
-let sumStats = {};
-
-summoners = ['Gabszao', 'Rusbrus', 'Hevnie'];
-refreshStats = false;
+const summoners = ['Gabszao', 'Rusbrus', 'Hevnie', 'Homemdosako'];
+const refreshStats = false;
 
 app.get('/', async (req, res) => {
-  let sumList = [];
-  // async/await does not work as expected with forEach().
-  try {
-    for (let summoner of summoners) {
-      let summonerStat = await opgg.getStats(summoner, {
-        region: 'br',
-        refresh: refreshStats,
-      });
-      sumList.push(summonerStat);
-    }
+  let summonerArray = [];
+  for (let summoner of summoners) {
+    try {
+      let summonerHeader = await getSummonerIDByName(summoner);
 
-    res.render('list', { list: sumList });
-  } catch (error) {
-    console.log('Error Occurred', error);
+      let summonerDetail = await getSummonerInfoByID(summonerHeader.id);
+
+      let summonerSoloQ = summonerDetail[0];
+      let summonerFlex = summonerDetail[1];
+      let soloQRank = summonerSoloQ.tier + ' ' + summonerSoloQ.rank;
+      let flexRank = summonerFlex.tier + ' ' + summonerFlex.rank;
+      let summonerStats = {
+        name: summonerHeader.name,
+        level: summonerHeader.summonerLevel,
+        soloQRank: soloQRank,
+        soloQLP: summonerSoloQ.leaguePoints,
+        soloQWins: summonerSoloQ.wins,
+        soloQLosses: summonerSoloQ.losses,
+        flexRank: flexRank,
+        flexLP: summonerFlex.leaguePoints,
+        flexWins: summonerFlex.wins,
+        flexLosses: summonerFlex.losses,
+      };
+
+      summonerArray.push(summonerStats);
+    } catch (error) {
+      console.log(error);
+    }
   }
+  res.render('list', { summoners: summonerArray });
 });
 
+async function getSummonerIDByName(iSummonerName) {
+  let url =
+    config.get('summonerHeaderURL') +
+    iSummonerName +
+    '?api_key=' +
+    process.env.API_KEY;
+  let result = await axios.get(url);
+  return result.data;
+}
+
+async function getSummonerInfoByID(iID) {
+  let url =
+    config.get('summonerDetailURL') + iID + '?api_key=' + process.env.API_KEY;
+  let result = await axios.get(url);
+  return result.data;
+}
 // Look for environment variable 'PORT' (for Heroku), else use defined port.
 const PORT = process.env.PORT || 5000;
 
 // Listen to PORT
-// Using string literal top print message with PORT variable
+// Using string literal to print message with PORT variable
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
